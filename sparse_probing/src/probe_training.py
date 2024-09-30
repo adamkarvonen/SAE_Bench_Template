8700aa7b59af68c8ef490939af505e78e302ddf6
Adam Karvonen <adam.karvonen@gmail.com>
2024-09-30 11:35:07 -0500

    Separate logic for selecting topk features using mean diff

--- sparse_probing/src/probe_training.py
+++ sparse_probing/src/probe_training.py
@@ -24,9 +24,7 @@ class Probe(nn.Module):
 def prepare_probe_data(
     all_activations: dict[str, Float[torch.Tensor, "num_datapoints d_model"]],
     class_idx: str,
-    batch_size: int,
-    select_top_k: Optional[int] = None,  # experimental feature
-) -> tuple[list[Float[torch.Tensor, "batch_size d_model"]], list[Int[torch.Tensor, "batch_size"]]]:
+) -> tuple[Float[torch.Tensor, "batch_size d_model"], Int[torch.Tensor, "batch_size"]]:
     positive_acts_BD = all_activations[class_idx]
     device = positive_acts_BD.device
 
@@ -46,30 +44,8 @@ def prepare_probe_data(
 
     assert selected_negative_acts_BD.shape == positive_acts_BD.shape
 
-    # Experimental feature: find the top k features that differ the most between in distribution and out of distribution
-    # zero out the rest. Useful for k-sparse probing experiments.
-    if select_top_k is not None:
-        positive_distribution_D = positive_acts_BD.mean(dim=(0))
-        negative_distribution_D = negative_acts.mean(dim=(0))
-        distribution_diff_D = (positive_distribution_D - negative_distribution_D).abs()
-        top_k_indices_D = torch.argsort(distribution_diff_D, descending=True)[:select_top_k]
-
-        mask_D = torch.ones(
-            distribution_diff_D.shape[0], dtype=torch.bool, device=positive_acts_BD.device
-        )
-        mask_D[top_k_indices_D] = False
-
-        masked_positive_acts_BD = positive_acts_BD.clone()
-        masked_negative_acts_BD = selected_negative_acts_BD.clone()
-
-        masked_positive_acts_BD[:, mask_D] = 0.0
-        masked_negative_acts_BD[:, mask_D] = 0.0
-    else:
-        masked_positive_acts_BD = positive_acts_BD
-        masked_negative_acts_BD = selected_negative_acts_BD
-
     # Combine positive and negative samples
-    combined_acts = torch.cat([masked_positive_acts_BD, masked_negative_acts_BD])
+    combined_acts = torch.cat([positive_acts_BD, selected_negative_acts_BD])
 
     combined_labels = torch.empty(len(combined_acts), dtype=torch.int, device=device)
     combined_labels[:num_positive] = dataset_info.POSITIVE_CLASS_LABEL
@@ -80,25 +56,38 @@ def prepare_probe_data(
     shuffled_acts = combined_acts[shuffle_indices]
     shuffled_labels = combined_labels[shuffle_indices]
 
-    # Reshape into lists of tensors with specified batch_size
-    num_samples = len(shuffled_acts)
-    num_batches = num_samples // batch_size
+    return shuffled_acts, shuffled_labels
 
-    batched_acts = [
-        shuffled_acts[i * batch_size : (i + 1) * batch_size] for i in range(num_batches)
-    ]
-    batched_labels = [
-        shuffled_labels[i * batch_size : (i + 1) * batch_size] for i in range(num_batches)
-    ]
 
-    return batched_acts, batched_labels
+@jaxtyped(typechecker=beartype)
+def select_top_k_mean_diff(
+    acts_BD: Float[torch.Tensor, "batch_size d_model"],
+    labels_B: Int[torch.Tensor, "batch_size"],
+    k: int,
+) -> Float[torch.Tensor, "batch_size d_model"]:
+    positive_mask_B = labels_B == dataset_info.POSITIVE_CLASS_LABEL
+    negative_mask_B = labels_B == dataset_info.NEGATIVE_CLASS_LABEL
+
+    positive_distribution_D = acts_BD[positive_mask_B].mean(dim=0)
+    negative_distribution_D = acts_BD[negative_mask_B].mean(dim=0)
+    distribution_diff_D = (positive_distribution_D - negative_distribution_D).abs()
+    top_k_indices_D = torch.argsort(distribution_diff_D, descending=True)[:k]
+
+    mask_D = torch.ones(acts_BD.shape[1], dtype=torch.bool, device=acts_BD.device)
+    mask_D[top_k_indices_D] = False
+
+    masked_acts_BD = acts_BD.clone()
+    masked_acts_BD[:, mask_D] = 0.0
+
+    return masked_acts_BD
 
 
 @jaxtyped(typechecker=beartype)
 @torch.no_grad
 def test_probe(
-    input_batches: list[Float[torch.Tensor, "batch_size d_model"]],
-    label_batches: list[Int[torch.Tensor, "batch_size"]],
+    inputs: Float[torch.Tensor, "test_dataset_size d_model"],
+    labels: Int[torch.Tensor, "test_dataset_size"],
+    batch_size: int,
     probe: Probe,
 ) -> float:
     criterion = nn.BCEWithLogitsLoss()
@@ -109,7 +98,9 @@ def test_probe(
         all_corrects = []
         losses = []
 
-        for acts_BD, labels_B in zip(input_batches, label_batches):
+        for i in range(0, len(labels), batch_size):
+            acts_BD = inputs[i : i + batch_size]
+            labels_B = labels[i : i + batch_size]
             logits_B = probe(acts_BD)
             preds_B = (logits_B > 0.0).long()
             correct_B = (preds_B == labels_B).float()
@@ -124,18 +115,19 @@ def test_probe(
         accuracy_all = torch.cat(all_corrects).mean().item()
         accuracy_0 = torch.cat(corrects_0).mean().item() if corrects_0 else 0.0
         accuracy_1 = torch.cat(corrects_1).mean().item() if corrects_1 else 0.0
-        loss = torch.stack(losses).mean().item()
+        all_loss = torch.stack(losses).mean().item()
 
     return accuracy_all
 
 
 @jaxtyped(typechecker=beartype)
 def train_probe(
-    train_input_batches: list[Float[torch.Tensor, "batch_size d_model"]],
-    train_label_batches: list[Int[torch.Tensor, "batch_size"]],
-    test_input_batches: list[Float[torch.Tensor, "batch_size d_model"]],
-    test_label_batches: list[Int[torch.Tensor, "batch_size"]],
+    train_inputs: Float[torch.Tensor, "train_dataset_size d_model"],
+    train_labels: Int[torch.Tensor, "train_dataset_size"],
+    test_inputs: Float[torch.Tensor, "test_dataset_size d_model"],
+    test_labels: Int[torch.Tensor, "test_dataset_size"],
     dim: int,
+    batch_size: int,
     epochs: int,
     device: str,
     model_dtype: torch.dtype,
@@ -147,7 +139,9 @@ def train_probe(
     criterion = nn.BCEWithLogitsLoss()
 
     for epoch in range(epochs):
-        for acts_BD, labels_B in zip(train_input_batches, train_label_batches):
+        for i in range(0, len(train_inputs), batch_size):
+            acts_BD = train_inputs[i : i + batch_size]
+            labels_B = train_labels[i : i + batch_size]
             logits_B = probe(acts_BD)
             loss = criterion(
                 logits_B, labels_B.clone().detach().to(device=device, dtype=model_dtype)
@@ -157,9 +151,9 @@ def train_probe(
             loss.backward()
             optimizer.step()
 
-        train_accuracy = test_probe(train_input_batches, train_label_batches, probe)
+        train_accuracy = test_probe(train_inputs, train_labels, batch_size, probe)
 
-        test_accuracy = test_probe(test_input_batches, test_label_batches, probe)
+        test_accuracy = test_probe(test_inputs, test_labels, batch_size, probe)
 
         if epoch == epochs - 1 and verbose:
             print(
@@ -171,8 +165,8 @@ def train_probe(
 
 @jaxtyped(typechecker=beartype)
 def train_probe_on_activations(
-    train_activations: dict[str, Float[torch.Tensor, "num_datapoints d_model"]],
-    test_activations: dict[str, Float[torch.Tensor, "num_datapoints d_model"]],
+    train_activations: dict[str, Float[torch.Tensor, "train_dataset_size d_model"]],
+    test_activations: dict[str, Float[torch.Tensor, "test_dataset_size d_model"]],
     probe_batch_size: int,
     epochs: int,
     lr: float,
@@ -185,15 +179,15 @@ def train_probe_on_activations(
     probes, test_accuracies = {}, {}
 
     for profession in train_activations.keys():
-        train_acts, train_labels = prepare_probe_data(
-            train_activations, profession, probe_batch_size, select_top_k
-        )
+        train_acts, train_labels = prepare_probe_data(train_activations, profession)
 
-        test_acts, test_labels = prepare_probe_data(
-            test_activations, profession, probe_batch_size, select_top_k
-        )
+        test_acts, test_labels = prepare_probe_data(test_activations, profession)
 
-        activation_dim = train_acts[0].shape[1]
+        if select_top_k is not None:
+            train_acts = select_top_k_mean_diff(train_acts, train_labels, select_top_k)
+            test_acts = select_top_k_mean_diff(test_acts, test_labels, select_top_k)
+
+        activation_dim = train_acts.shape[1]
 
         print(f"activation dim: {activation_dim}")
 
@@ -202,8 +196,9 @@ def train_probe_on_activations(
             train_labels,
             test_acts,
             test_labels,
-            epochs=epochs,
             dim=activation_dim,
+            batch_size=probe_batch_size,
+            epochs=epochs,
             device=device,
             model_dtype=model_dtype,
             lr=lr,
