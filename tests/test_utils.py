47af3662baee348f7d70afd99819dae5a2663768
Adam Karvonen <adam.karvonen@gmail.com>
2024-10-04 16:00:07 -0500

    Add function to average results from multiple runs

---
+++ tests/test_utils.py
@@ -0,0 +1,28 @@
+import utils.formatting_utils as formatting_utils
+import utils.testing_utils as testing_utils
+
+
+def test_average_results():
+    # Prepare test data
+    results_dict = {
+        "dataset1_results": {
+            "sae1": {"accuracy": 0.8, "loss": 0.5},
+            "sae2": {"accuracy": 0.75, "loss": 0.6},
+        },
+        "dataset2_results": {
+            "sae1": {"accuracy": 0.85, "loss": 0.4},
+            "sae2": {"accuracy": 0.7, "loss": 0.65},
+        },
+    }
+    dataset_names = ["dataset1", "dataset2"]
+
+    # Expected output
+    expected_output = {
+        "sae1": {"accuracy": 0.825, "loss": 0.45},
+        "sae2": {"accuracy": 0.725, "loss": 0.625},
+    }
+
+    # Call the function
+    output = formatting_utils.average_results_dictionaries(results_dict, dataset_names)
+
+    testing_utils.compare_dicts_within_tolerance(output, expected_output, tolerance=1e-6)
