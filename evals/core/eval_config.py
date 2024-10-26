9b1dd45af4ae0b67e69a73e4e1d87d64976dd0cc
Curt Tigges <curttigges@Curts-MacBook-Pro.local>
2024-10-26 15:08:03 -0700

    Added core evals to repo

---
+++ evals/core/eval_config.py
@@ -0,0 +1,72 @@
+from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
+import subprocess
+from typing import Optional
+from pydantic.dataclasses import dataclass
+from pydantic import Field
+from evals.base_eval_output import BaseEvalConfig
+from sae_lens import SAEConfig
+
+# Define the eval config, which inherits from BaseEvalConfig, and include fields with titles and descriptions.
+@dataclass
+class CoreEvalConfig(BaseEvalConfig):
+    batch_size_prompts: int = Field(
+        default=16,
+        title="Batch Size Prompts",
+        description="Batch size for evaluation prompts",
+    )
+    n_eval_reconstruction_batches: int = Field(
+        default=10,
+        title="Reconstruction Batches",
+        description="Number of evaluation batches for reconstruction metrics",
+    )
+    n_eval_sparsity_variance_batches: int = Field(
+        default=1,
+        title="Sparsity Variance Batches",
+        description="Number of evaluation batches for sparsity and variance metrics",
+    )
+    dataset: str = Field(
+        default="Skylion007/openwebtext",
+        title="Dataset",
+        description="Dataset to evaluate on",
+    )
+    context_size: int = Field(
+        default=128,
+        title="Context Length",
+        description="Context length to evaluate on",
+    )
+    compute_kl: bool = Field(
+        default=False,
+        title="Compute KL",
+        description="Compute KL divergence",
+    )
+    compute_ce_loss: bool = Field(
+        default=False,
+        title="Compute CE Loss",
+        description="Compute cross-entropy loss",
+    )
+    compute_l2_norms: bool = Field(
+        default=False,
+        title="Compute L2 Norms",
+        description="Compute L2 norms",
+    )
+    compute_sparsity_metrics: bool = Field(
+        default=False,
+        title="Compute Sparsity Metrics",
+        description="Compute sparsity metrics",
+    )
+    compute_variance_metrics: bool = Field(
+        default=False,
+        title="Compute Variance Metrics",
+        description="Compute variance metrics",
+    )
+    compute_featurewise_density_statistics: bool = Field(
+        default=False,
+        title="Compute Featurewise Density Statistics",
+        description="Compute featurewise density statistics",
+    )
+    compute_featurewise_weight_based_metrics: bool = Field(
+        default=False,
+        title="Compute Featurewise Weight-Based Metrics",
+        description="Compute featurewise weight-based metrics",
+    )
