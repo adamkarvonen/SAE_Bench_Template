df4c5ee91a6d736177d8bda0351bf356d799114c
Curt Tigges <curttigges@Curts-MacBook-Pro.local>
2024-11-05 14:39:56 -0800

    Added tests for core evals and updated config/output

--- evals/core/eval_config.py
+++ evals/core/eval_config.py
@@ -1,40 +1,40 @@
 from pydantic.dataclasses import dataclass
 from pydantic import Field
-from evals.base_eval_output import DEFAULT_DISPLAY, BaseEvalConfig
+from evals.base_eval_output import BaseEvalConfig
 
 
 # Define the eval config, which inherits from BaseEvalConfig, and include fields with titles and descriptions.
 @dataclass
 class CoreEvalConfig(BaseEvalConfig):
+    model_name: str = Field(
+        default="pythia-70m-deduped",
+        title="Model Name",
+        description="Model name",
+    )
     batch_size_prompts: int = Field(
         default=16,
         title="Batch Size Prompts",
         description="Batch size for evaluation prompts",
-        json_schema_extra=DEFAULT_DISPLAY,
     )
     n_eval_reconstruction_batches: int = Field(
         default=10,
         title="Reconstruction Batches",
         description="Number of evaluation batches for reconstruction metrics",
-        json_schema_extra=DEFAULT_DISPLAY,
     )
     n_eval_sparsity_variance_batches: int = Field(
         default=1,
         title="Sparsity Variance Batches",
         description="Number of evaluation batches for sparsity and variance metrics",
-        json_schema_extra=DEFAULT_DISPLAY,
     )
     dataset: str = Field(
         default="Skylion007/openwebtext",
         title="Dataset",
         description="Dataset to evaluate on",
-        json_schema_extra=DEFAULT_DISPLAY,
     )
     context_size: int = Field(
         default=128,
         title="Context Length",
         description="Context length to evaluate on",
-        json_schema_extra=DEFAULT_DISPLAY,
     )
     compute_kl: bool = Field(
         default=False,
@@ -71,3 +71,9 @@ class CoreEvalConfig(BaseEvalConfig):
         title="Compute Featurewise Weight-Based Metrics",
         description="Compute featurewise weight-based metrics",
     )
+    verbose: bool = Field(
+        default=False,
+        title="Verbose",
+        description="Enable verbose output",
+    )
+
