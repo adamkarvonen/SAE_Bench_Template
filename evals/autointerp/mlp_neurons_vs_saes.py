d528f3bb026387d3fabc382d3a29e5144ab045e5
Adam Karvonen <adam.karvonen@gmail.com>
2024-11-17 20:26:19 +0000

    Also compare residual stream as a baseline

--- evals/autointerp/mlp_neurons_vs_saes.py
+++ evals/autointerp/mlp_neurons_vs_saes.py
@@ -6,7 +6,8 @@ import sae_bench_utils.general_utils as general_utils
 if __name__ == "__main__":
     model_name = "gemma-2-2b"
     layers = [5, 12, 19]
-    d_model = 9216
+    d_model = 2304
+    d_mlp = d_model * 4
     llm_dtype = "bfloat16"
     llm_batch_size = 32
     RANDOM_SEED = 42
@@ -18,9 +19,17 @@ if __name__ == "__main__":
 
     for layer in layers:
         mlp_hook = f"blocks.{layer}.mlp.hook_post"
-        mlp_neurons = identity_sae.IdentitySAE(model_name, d_model, layer, hook_name=mlp_hook)
+        mlp_neurons = identity_sae.IdentitySAE(model_name, d_mlp, layer, hook_name=mlp_hook)
 
-        selected_saes = [(f"identity_mlp_{model_name}_layer_{layer}", mlp_neurons)]
+        resid_hook = f"blocks.{layer}.hook_resid_post"
+        resid_dimensions = identity_sae.IdentitySAE(
+            model_name, d_model, layer, hook_name=resid_hook
+        )
+
+        selected_saes = [
+            (f"identity_mlp_{model_name}_layer_{layer}", mlp_neurons),
+            (f"identity_resid_{model_name}_layer_{layer}", resid_dimensions),
+        ]
 
         autointerp.run_eval(
             autointerp.AutoInterpEvalConfig(
@@ -37,11 +46,14 @@ if __name__ == "__main__":
         )
 
     for layer in layers:
-        sae_regex_pattern = r"gemma-scope-2b-pt-mlp"
-        sae_block_pattern = rf".*layer_{layer}.*(16k).*"
+        sae_regex_patterns = [
+            r"gemma-scope-2b-pt-mlp-canonical",
+            r"gemma-scope-2b-pt-res-canonical",
+        ]
+        sae_block_patterns = [rf".*layer_{layer}.*(16k).*", rf".*layer_{layer}.*(16k).*"]
 
-        selected_saes = sae_selection_utils.get_saes_from_regex(
-            sae_regex_pattern, sae_block_pattern
+        selected_saes = sae_selection_utils.select_saes_multiple_patterns(
+            sae_regex_patterns, sae_block_patterns
         )
 
         autointerp.run_eval(
