edd0de209f2dc7deef039281b672037d906ea452
Adam Karvonen <adam.karvonen@gmail.com>
2024-11-17 22:21:54 +0000

    Also check for b_mag so we don't ignore gated SAEs bias

--- evals/core/main.py
+++ evals/core/main.py
@@ -334,7 +334,7 @@ def get_featurewise_weight_based_metrics(sae: SAE) -> dict[str, Any]:
     encoder_norms = sae.W_enc.norm(dim=-2).cpu().tolist()
 
     # gated models have a different bias (no b_enc)
-    if not hasattr(sae, "b_enc"):
+    if not hasattr(sae, "b_enc") and not hasattr(sae, "b_mag"):
         encoder_bias = torch.zeros(sae.cfg.d_sae).cpu().tolist()
     elif sae.cfg.architecture != "gated":
         encoder_bias = sae.b_enc.cpu().tolist()
